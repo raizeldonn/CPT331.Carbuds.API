dd84104111392ef425deb7f5164f794f41aae997
Rolando <chancellor.rolando@gmail.com>
2021-03-27 14:10:39 +1100

    added components for Login

---
+++ CPT331.Carbuds.Api/Contracts/Auth/PostLoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPT331.Carbuds.Api.Contracts.Auth
+{
+  public class PostLoginResponse: BaseResponse
+  {
+    public string IdToken { get; set; }
+    public long TokenExpiresIn { get; set; }
+  }
+}
