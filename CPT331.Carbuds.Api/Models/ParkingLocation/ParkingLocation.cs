e602ab8d0ae8141fdafeac8bf1701681ad46414d
s3572278 <s3572278@student.rmit.edu.au>
2021-04-11 11:00:20 +1000

    - Created Dynamo Table - Added Contract, Controller, Model and Service files for Parking Locations.

---
+++ CPT331.Carbuds.Api/Models/ParkingLocation/ParkingLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPT331.Carbuds.Api.Models.ParkingLocation
+{
+    public class ParkingLocation
+    {
+        public string Location { get; set; }
+        public string FriendlyName { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public int Postcode { get; set; }
+        public string State { get; set; }
+    }
+}
