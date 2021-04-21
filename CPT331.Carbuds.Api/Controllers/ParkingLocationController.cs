bfc3e9da319f8d4cf20fdf80dc97b833a7e3c99a
Rolando <chancellor.rolando@gmail.com>
2021-04-21 20:09:52 +1000

    add endpoints and contracts to delete parking location

--- CPT331.Carbuds.Api/Controllers/ParkingLocationController.cs
+++ CPT331.Carbuds.Api/Controllers/ParkingLocationController.cs
@@ -10,57 +10,77 @@ using System.Threading.Tasks;
 
 namespace CPT331.Carbuds.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ParkingLocationController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParkingLocationController : ControllerBase
+  {
+    private IParkingLocationService _parkingLocationService;
+    public ParkingLocationController(IParkingLocationService locationService)
     {
-        private IParkingLocationService _parkingLocationService;
-        public ParkingLocationController(IParkingLocationService locationService)
-        {
-            _parkingLocationService = locationService;
-        }
-
-        [HttpPost]
-        public async Task<PostAddUpdateParkingLocationResponse> AddUpdateParkingLocation(PostAddUpdateParkingLocationRequest request)
-        {
-            try
-            {
-                return new PostAddUpdateParkingLocationResponse()
-                {
-                    Success = await _parkingLocationService.AddUpdateParkingLocation(request.Parking)
-                };
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Exception encountered in POST to /api/parkingLocations: {JsonConvert.SerializeObject(e)}");
-                return new PostAddUpdateParkingLocationResponse()
-                {
-                    Success = false,
-                    ErrorMessage = e.Message
-                };
-            }
-        }
-
-        [HttpGet("list")]
-        public async Task<GetListParkingLocationsResponse> ListAllParkingLocations()
-        {
-            try
-            {
-                return new GetListParkingLocationsResponse()
-                {
-                    Success = true,
-                    ParkingLocations = await _parkingLocationService.ListAllParkingLocations()
-                };
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Exception encountered in GET to /api/parkingLocations/list: {JsonConvert.SerializeObject(e)}");
-                return new GetListParkingLocationsResponse()
-                {
-                    Success = false,
-                    ErrorMessage = e.Message
-                };
-            }
-        }
+      _parkingLocationService = locationService;
     }
+
+    [HttpPost]
+    public async Task<PostAddUpdateParkingLocationResponse> AddUpdateParkingLocation(PostAddUpdateParkingLocationRequest request)
+    {
+      try
+      {
+        return new PostAddUpdateParkingLocationResponse()
+        {
+          Success = await _parkingLocationService.AddUpdateParkingLocation(request.Parking)
+        };
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"Exception encountered in POST to /api/parkingLocations: {JsonConvert.SerializeObject(e)}");
+        return new PostAddUpdateParkingLocationResponse()
+        {
+          Success = false,
+          ErrorMessage = e.Message
+        };
+      }
+    }
+
+    [HttpGet("list")]
+    public async Task<GetListParkingLocationsResponse> ListAllParkingLocations()
+    {
+      try
+      {
+        return new GetListParkingLocationsResponse()
+        {
+          Success = true,
+          ParkingLocations = await _parkingLocationService.ListAllParkingLocations()
+        };
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"Exception encountered in GET to /api/parkingLocations/list: {JsonConvert.SerializeObject(e)}");
+        return new GetListParkingLocationsResponse()
+        {
+          Success = false,
+          ErrorMessage = e.Message
+        };
+      }
+    }
+
+    [HttpDelete]
+    public async Task<DeleteParkingLocationResponse> DeleteParkingLocation(DeleteParkingLocationRequest request)
+    {
+      try
+      {
+        return new DeleteParkingLocationResponse()
+        {
+          Success = await _parkingLocationService.DeleteParkingLocation(request.LocationUuid)
+        };
+      }
+      catch(Exception e)
+      {
+        return new DeleteParkingLocationResponse()
+        {
+          Success = false,
+          ErrorMessage = e.Message
+        };
+      }
+    }
+  }
 }
--- CPT331.Carbuds.Api/Services/ParkingLocationService.cs
+++ CPT331.Carbuds.Api/Services/ParkingLocationService.cs
@@ -9,57 +9,72 @@ using Microsoft.Extensions.Configuration;
 
 namespace CPT331.Carbuds.Api.Services
 {
-    public interface IParkingLocationService
+  public interface IParkingLocationService
+  {
+    // get a list of parking locations from database
+    Task<List<ParkingLocation>> ListAllParkingLocations();
+    // add parking location in database
+    Task<bool> AddUpdateParkingLocation(ParkingLocation record);
+    Task<bool> DeleteParkingLocation(string parkingLocationUuid);
+  }
+
+  public class ParkingLocationService : IParkingLocationService
+  {
+    private IAmazonDynamoDB _dynamoDb;
+    private IConfiguration _config;
+    private IUtilityService _utils;
+
+    // constructor
+    public ParkingLocationService(IAmazonDynamoDB dynamoDb, IConfiguration config, IUtilityService utils)
     {
-        // get a list of parking locations from database
-        Task<List<ParkingLocation>> ListAllParkingLocations();
-        // add parking location in database
-        Task<bool> AddUpdateParkingLocation(ParkingLocation record);
+      _dynamoDb = dynamoDb;
+      _config = config;
+      _utils = utils;
     }
 
-    public class ParkingLocationService: IParkingLocationService
+    // method to retrieve a list of parking locations from database
+    public async Task<List<ParkingLocation>> ListAllParkingLocations()
     {
-        private IAmazonDynamoDB _dynamoDb;
-        private IConfiguration _config;
-        private IUtilityService _utils;
+      var parkingLocationList = new List<ParkingLocation>();
+      ScanRequest scanReq = new ScanRequest()
+      {
+        TableName = _config.GetValue<string>("DynamoDb:Tablenames:ParkingLocations")
+      };
 
-        // constructor
-        public ParkingLocationService(IAmazonDynamoDB dynamoDb, IConfiguration config, IUtilityService utils)
-        {
-            _dynamoDb = dynamoDb;
-            _config = config;
-            _utils = utils;
-        }
+      var dbResult = await _dynamoDb.ScanAsync(scanReq);
+      foreach (var item in dbResult.Items)
+      {
+        parkingLocationList.Add(_utils.ToObjectFromDynamoResult<ParkingLocation>(item));
+      }
 
-        // method to retrieve a list of parking locations from database
-        public async Task<List<ParkingLocation>> ListAllParkingLocations()
-        {
-            var parkingLocationList = new List<ParkingLocation>();
-            ScanRequest scanReq = new ScanRequest()
-            {
-                TableName = _config.GetValue<string>("DynamoDb:Tablenames:ParkingLocations")
-            };
+      return parkingLocationList;
 
-            var dbResult = await _dynamoDb.ScanAsync(scanReq);
-            foreach (var item in dbResult.Items)
-            {
-                parkingLocationList.Add(_utils.ToObjectFromDynamoResult<ParkingLocation>(item));
-            }
-
-            return parkingLocationList;
-
-        }
-
-        // method to add a parking location to database
-        public async Task<bool> AddUpdateParkingLocation(ParkingLocation record)
-        {
-            var putReq = new PutItemRequest()
-            {
-                TableName = _config.GetValue<string>("DynamoDb:Tablenames:ParkingLocations"),
-                Item = _utils.ToDynamoAttributeValueDictionary<ParkingLocation>(record)
-            };
-            var response = await _dynamoDb.PutItemAsync(putReq);
-            return true;
-        }
     }
+
+    // method to add a parking location to database
+    public async Task<bool> AddUpdateParkingLocation(ParkingLocation record)
+    {
+      var putReq = new PutItemRequest()
+      {
+        TableName = _config.GetValue<string>("DynamoDb:Tablenames:ParkingLocations"),
+        Item = _utils.ToDynamoAttributeValueDictionary<ParkingLocation>(record)
+      };
+      var response = await _dynamoDb.PutItemAsync(putReq);
+      return true;
+    }
+
+    public async Task<bool> DeleteParkingLocation(string parkingLocationUuid)
+    {
+      var delReq = new DeleteItemRequest()
+      {
+        TableName = _config.GetValue<string>("DynamoDb:Tablenames:ParkingLocations"),
+        Key = new Dictionary<string, AttributeValue>() {
+            { "Uuid", new AttributeValue { S = parkingLocationUuid } },
+        }
+      };
+
+      var response = await _dynamoDb.DeleteItemAsync(delReq);
+      return true;
+    }
+  }
 }
