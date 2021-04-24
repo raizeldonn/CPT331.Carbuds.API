18ec9070ed5821d6f86fc98c26b8c1655f4862dc
Rolando <chancellor.rolando@gmail.com>
2021-04-24 12:57:23 +1000

    CORS configuration for API

--- CPT331.Carbuds.Api/Startup.cs
+++ CPT331.Carbuds.Api/Startup.cs
@@ -15,54 +15,63 @@ using Microsoft.Extensions.Logging;
 
 namespace CPT331.Carbuds.Api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public static IConfiguration Configuration { get; private set; }
-
-        // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-
-            services.AddAWSService<Amazon.DynamoDBv2.IAmazonDynamoDB>();
-            services.AddAWSService<Amazon.CognitoIdentityProvider.IAmazonCognitoIdentityProvider>();
-
-            services.AddSingleton<IUtilityService, UtilityService>();
-            services.AddSingleton<IAuthService, AuthService>();
-            services.AddSingleton<IUserService, UserService>();
-            services.AddSingleton<IParkingAllocationService, ParkingAllocationService>();
-            services.AddSingleton<ICarService, CarService>();
-            services.AddSingleton<IParkingLocationService, ParkingLocationService>();
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapGet("/", async context =>
-                {
-                        await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
-                    });
-            });
-        }
+      Configuration = configuration;
     }
+
+    public static IConfiguration Configuration { get; private set; }
+    public const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy(MyAllowSpecificOrigins,
+        builder => builder.WithOrigins("*"));
+      });
+
+
+      services.AddAWSService<Amazon.DynamoDBv2.IAmazonDynamoDB>();
+      services.AddAWSService<Amazon.CognitoIdentityProvider.IAmazonCognitoIdentityProvider>();
+
+      services.AddSingleton<IUtilityService, UtilityService>();
+      services.AddSingleton<IAuthService, AuthService>();
+      services.AddSingleton<IUserService, UserService>();
+      services.AddSingleton<IParkingAllocationService, ParkingAllocationService>();
+      services.AddSingleton<ICarService, CarService>();
+      services.AddSingleton<IParkingLocationService, ParkingLocationService>();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseCors(MyAllowSpecificOrigins);
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+        endpoints.MapGet("/", async context =>
+              {
+            await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+          });
+      });
+    }
+  }
 }
